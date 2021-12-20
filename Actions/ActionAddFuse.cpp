916d236101d9c87629426165353ebcb7ea9e57e1
Mohamed Ezzat <moezemara@gmail.com>
2021-12-20 22:23:30 +0200

    Revert "Revert "Validated drawing area""
    This reverts commit 1534c3d07aa1e3874a7afeee2f764ccb7c1c7756.

--- Actions/ActionAddFuse.cpp
+++ Actions/ActionAddFuse.cpp
@@ -25,19 +25,23 @@ void ActionAddFuse::Execute()
 	pUI->ClearStatusBar();
 
 
-	GraphicsInfo* pGInfo = new GraphicsInfo(2); //Gfx info to be used to construct the Comp
+	if (Cx < 900 && Cx > 20 && Cy >= 130 && Cy < 550) {
 
-	//Calculate the rectangle Corners
-	int compWidth = pUI->getCompWidth();
-	int compHeight = pUI->getCompHeight();
+		GraphicsInfo* pGInfo = new GraphicsInfo(2); //Gfx info to be used to construct the Comp
 
-	pGInfo->PointsList[0].x = Cx - compWidth / 2;
-	pGInfo->PointsList[0].y = Cy - compHeight / 2;
-	pGInfo->PointsList[1].x = Cx + compWidth / 2;
-	pGInfo->PointsList[1].y = Cy + compHeight / 2;
+		//Calculate the rectangle Corners
+		int compWidth = pUI->getCompWidth();
+		int compHeight = pUI->getCompHeight();
 
-	Fuse* pR = new Fuse(pGInfo);
-	pManager->AddComponent(pR);
+		pGInfo->PointsList[0].x = Cx - compWidth / 2;
+		pGInfo->PointsList[0].y = Cy - compHeight / 2;
+		pGInfo->PointsList[1].x = Cx + compWidth / 2;
+		pGInfo->PointsList[1].y = Cy + compHeight / 2;
+
+		Fuse* pR = new Fuse(pGInfo);
+		pManager->AddComponent(pR);
+	}
+	else pUI->PrintMsg("Cannot draw except in the drawing area!");
 }
 
 void ActionAddFuse::Undo()
