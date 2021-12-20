3489bf437ef83c9a8b88aaa525a1c3b8687c473f
ahmedhesham47 <ahmedheshamgalal47@gmail.com>
2021-12-20 02:47:22 +0200

    Validated drawing area
    Validated each component to draw only inside the indicated drawing area boundaries

--- Actions/ActionAddRes.cpp
+++ Actions/ActionAddRes.cpp
@@ -11,7 +11,7 @@ ActionAddRes::~ActionAddRes(void)
 
 void ActionAddRes::Execute()
 {
-	
+
 	//Get a Pointer to the user Interfaces
 	UI* pUI = pManager->GetUI();
 
@@ -22,22 +22,25 @@ void ActionAddRes::Execute()
 	pUI->GetPointClicked(Cx, Cy);
 
 	//Clear Status Bar
-	pUI->ClearStatusBar();	
-	
-	
-	GraphicsInfo * pGInfo= new GraphicsInfo(2); //Gfx info to be used to construct the Comp
-	
-	//Calculate the rectangle Corners
-	int compWidth = pUI->getCompWidth();
-	int compHeight = pUI->getCompHeight();
+	pUI->ClearStatusBar();
 
-	pGInfo->PointsList[0].x = Cx - compWidth/2;
-	pGInfo->PointsList[0].y = Cy - compHeight/2;
-	pGInfo->PointsList[1].x = Cx + compWidth/2;
-	pGInfo->PointsList[1].y = Cy + compHeight/2;
-	 
-	Resistor* pR = new Resistor(pGInfo);
-	pManager->AddComponent(pR);
+	if (Cx < 900 && Cx > 20 && Cy >= 130 && Cy < 550) {
+
+		GraphicsInfo* pGInfo = new GraphicsInfo(2); //Gfx info to be used to construct the Comp
+
+		//Calculate the rectangle Corners
+		int compWidth = pUI->getCompWidth();
+		int compHeight = pUI->getCompHeight();
+
+		pGInfo->PointsList[0].x = Cx - compWidth / 2;
+		pGInfo->PointsList[0].y = Cy - compHeight / 2;
+		pGInfo->PointsList[1].x = Cx + compWidth / 2;
+		pGInfo->PointsList[1].y = Cy + compHeight / 2;
+
+		Resistor* pR = new Resistor(pGInfo);
+		pManager->AddComponent(pR);
+	}
+	else pUI->PrintMsg("Cannot draw except in the drawing area!");
 }
 
 void ActionAddRes::Undo()
--- Actions/ActionAddSwitch.cpp
+++ Actions/ActionAddSwitch.cpp
@@ -25,19 +25,23 @@ void ActionAddSwitch::Execute()
 	pUI->ClearStatusBar();
 
 
-	GraphicsInfo* pGInfo = new GraphicsInfo(2); //Gfx info to be used to construct the Comp
+	if (Cx < 900 && Cx > 20 && Cy >= 130 && Cy < 550) {
 
-	//Calculate the rectangle Corners
-	int compWidth = pUI->getCompWidth();
-	int compHeight = pUI->getCompHeight();
+		GraphicsInfo* pGInfo = new GraphicsInfo(2); //Gfx info to be used to construct the Comp
 
-	pGInfo->PointsList[0].x = Cx - compWidth / 2;
-	pGInfo->PointsList[0].y = Cy - compHeight / 2;
-	pGInfo->PointsList[1].x = Cx + compWidth / 2;
-	pGInfo->PointsList[1].y = Cy + compHeight / 2;
+		//Calculate the rectangle Corners
+		int compWidth = pUI->getCompWidth();
+		int compHeight = pUI->getCompHeight();
 
-	Switch* pR = new Switch(pGInfo);
-	pManager->AddComponent(pR);
+		pGInfo->PointsList[0].x = Cx - compWidth / 2;
+		pGInfo->PointsList[0].y = Cy - compHeight / 2;
+		pGInfo->PointsList[1].x = Cx + compWidth / 2;
+		pGInfo->PointsList[1].y = Cy + compHeight / 2;
+
+		Switch* pR = new Switch(pGInfo);
+		pManager->AddComponent(pR);
+	}
+	else pUI->PrintMsg("Cannot draw except in the drawing area!");
 }
 
 void ActionAddSwitch::Undo()
