c4ba20cde20217dcb5f1a3acef102613c32b0fde
ahmedhesham47 <ahmedheshamgalal47@gmail.com>
2021-12-31 19:01:30 +0200

    Re-validated switch
    Fixed the switch as such it must only have a value of zero in the design mode. The user can later open it only in the simulation mode.

--- Actions/ActionSimWindow.cpp
+++ Actions/ActionSimWindow.cpp
@@ -20,7 +20,7 @@ bool ActionSimWindow::Validate() {
 					if (pManager->getCompValue(CompList[i]) > double(0) && pManager->getCompLabel(CompList[i]) != "Component") counter++;
 				}
 				if (pManager->isSwitch(CompList[i])) {
-					if ((pManager->getCompValue(CompList[i]) == double(0) || pManager->getCompValue(CompList[i]) == double(1)) && pManager->getCompLabel(CompList[i]) != "Component")counter++;
+					if ((pManager->getCompValue(CompList[i]) == double(0) && pManager->getCompLabel(CompList[i]) != "Component")counter++;
 				}
 				if(pManager->isGround(CompList[i])) {
 					if (pManager->getCompValue(CompList[i]) == double(0) && pManager->getCompLabel(CompList[i]) != "Component") counter++;
